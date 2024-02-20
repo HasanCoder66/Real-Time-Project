8f7802645218be7429ca48113ea06f39c9746868
HasanCoder66 <hasanashraf874@gmail.com>
2024-02-20 13:09:20 +0500

    particles not working

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Hero from "./Components/Hero/Hero";
+import MobileNav from "./Components/MobileNav/MobileNav";
+import Nav from "./Components/Nav/Nav";
 import './App.css'
+// import Services from "@/Components/Services";
+// import Skills from "@/Components/Skills";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [nav, setNav] = React.useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
   return (
-    <>
+    <div className="overflow-x-hidden">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* NAVBAR ===> */}
+        <MobileNav nav={nav} closeNav={closeNav} />
+        <Nav openNav={openNav} />
+        {/* HERO ===> */}
+        <Hero />
+        <div className="relative z-[30]" >
+          {/* <About /> */}
+          {/* <Services /> */}
+          {/* <Skills /> */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
--- my-project/src/Components/Hero/Hero.jsx
+++ my-project/src/Components/Hero/Hero.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 // import TextEffect from './TextEffect'
 // import Image from "next/image";
 import { ArrowDownTrayIcon, PlayCircleIcon } from '@heroicons/react/20/solid';
-import  Particle  from '../Particles/Particles';
+// import  Particle  from '../Particles/Particles';
 
 const Hero = () => {
   return (
     // bg-[url("/images/banner.jpg")]
-    <div className='h-[88vh]  mt-[10vh] bg-cover bg-center bg-yellow-400 '>
-        <Particle />
+    <div className='h-[88vh] bg-[url("/images/banner.jpg")]  mt-[10vh] bg-cover bg-center bg-yellow-400 '>
+        {/* <Particle /> */}
         <div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center ' >
             <div>
             <h1 className='text-[35px] md:text-[50px] text-white font-bold ' >
