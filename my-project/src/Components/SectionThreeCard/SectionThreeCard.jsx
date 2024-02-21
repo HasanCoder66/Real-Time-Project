2a8110eda7b376324d0911c2bdcb9945016e4947
HasanCoder66 <hasanashraf874@gmail.com>
2024-02-21 13:29:31 +0500

    cards work done

---
+++ my-project/src/Components/SectionThreeCard/SectionThreeCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+
+function SectionThreeCard({data}) {
+  return (
+    <Card sx={{ maxWidth: 345,  }}>
+      <CardActionArea>
+        <CardContent className='text-center'>
+          <Typography gutterBottom variant="h5" component="div" className='text-[#1e4649] ' >
+            {data?.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {data?.desc}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+export default SectionThreeCard
