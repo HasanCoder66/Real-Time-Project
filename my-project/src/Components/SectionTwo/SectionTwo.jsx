fd4c9e791587581f91271a793b3ef0b987603613
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-02-24 13:56:29 +0500

    kuch changes ki han

--- my-project/src/Components/SectionTwo/SectionTwo.jsx
+++ my-project/src/Components/SectionTwo/SectionTwo.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Spinner from "../Spinner/Spinner";
-import  Button from "../Button/Button";
+import Button from "../Button/Button";
 
 const SectionTwo = () => {
   return (
     <div className=" flex w-[100%]  h-[100%]  justify-center items-center flex-col md:flex-row  sm:text-center mt-[3rem]">
-      <div className=" mt-5 w-[70%] sm:flex-col ">
-      {/* text-[35px] md:text-[75px] text-[#18252b] font-bold md:ml-[8rem]  sm:w-[100%] sm:text-[55px] */}
-        <h1 className=" font-bold md:text-[75px]   text-[#1e4649] text-[55px] mt-[1rem]">STEERING 360 ON DIGITAL AVENUES</h1>
-        <p className="sm:text-[15px] md:text-[15px] text-[#18252b] text-left md:ml-[8rem] sm:w-[100]">
+      <div className=" mt-5 flex-1 sm:flex-col ">
+        {/* text-[35px] md:text-[75px] text-[#18252b] font-bold md:ml-[8rem]  sm:w-[100%] sm:text-[55px] */}
+        <h1 className=" font-bold md:text-[75px] text-[#1e4649] text-[55px] mt-[1rem]">STEERING 360 ON DIGITAL AVENUES</h1>
+        <p className="sm:text-[15px] md:text-[15px] text-[#18252b] text-center px-[50px] sm:w-[100]">
           We believe in transformations and make all accelerations to simplify
           the process and complete each task an easy breeze for you. Our aim is
           simple, to innovate, create and curate perfection through our
@@ -17,14 +17,14 @@ const SectionTwo = () => {
           growth.
         </p>
         <div>
-        <p className=" text-2xl text-[#1e4649] font-semibold mb-1 md:ml-[8rem] mt-[3rem] mb-[1rem] text-start">Start a Conversation</p>
-          <Button text='Talk to an Expert'/>
+          <p className=" text-2xl text-[#1e4649] font-semibold mb-1 md:ml-[8rem] mt-[3rem] mb-[1rem] text-start">Start a Conversation</p>
+          <Button text='Talk to an Expert' />
         </div>
-          
+
       </div>
-      <div className=" w-[30%] mt-5">
+      {/* <div className=" flex-1 mt-5">
         <Spinner />
-      </div>
+      </div> */}
     </div>
   );
 };
