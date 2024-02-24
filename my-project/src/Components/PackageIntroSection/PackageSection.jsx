1bb0e26f220981f6ba0d360fa52b1f6efb01acc7
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-02-24 17:53:49 +0500

    home responsive

--- my-project/src/Components/PackageIntroSection/PackageSection.jsx
+++ my-project/src/Components/PackageIntroSection/PackageSection.jsx
@@ -3,9 +3,9 @@ import PackageCard from '../PackageCard/PackageCard'
 
 const PackageSection = () => {
     return (
-        <div className='p-[60px]'>
+        <div className='lg:p-[60px]'>
             <h1 className='font-bold text-[36px] text-center my-[20px] text-[#1e4649]'>LET'S HAVE A LOOK AT WHAT GOES INTO ACCELERATING THE BUSINESS GROWTH OF OUR CLIENTS</h1>
-            <div className='flex gap-[10px] justify-evenly flex-col lg:flex-row '>
+            <div className='flex gap-[10px] justify-evenly flex-col lg:flex-row sm:items-center'>
                 <PackageCard />
                 <PackageCard />
                 <PackageCard />
