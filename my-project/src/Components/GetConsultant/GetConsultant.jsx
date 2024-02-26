df3599b72644a379c85701e109c46b06d71b18ab
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-02-26 13:03:03 +0500

    all pages responsive

--- my-project/src/Components/GetConsultant/GetConsultant.jsx
+++ my-project/src/Components/GetConsultant/GetConsultant.jsx
@@ -4,7 +4,7 @@ import TextareaCmp from "../TextAreaCmp/TextAreaCmp";
 
 const GetConsultant = () => {
   return (
-    <div className='p-[5rem] pr-[5rem] h-[100vh] bg-[url("https://res.cloudinary.com/dpvxkqhi8/image/upload/v1708534584/branding%20hopes/banner_b0pbcx.jpg")]  pt-[12vh] bg-cover bg-center bg-[#18252b] '>
+    <div className='lg:p-[5rem] lg:pr-[5rem] px-[2rem] pb-[3rem] h-[100vh] bg-[url("https://res.cloudinary.com/dpvxkqhi8/image/upload/v1708534584/branding%20hopes/banner_b0pbcx.jpg")]  pt-[12vh] bg-cover bg-center bg-[#18252b] '>
       <div>
         <h1 className="text-[35px] md:text-[50px] text-yellow-300  font-bold ">
           Get
@@ -15,7 +15,7 @@ const GetConsultant = () => {
         <p className="mt-[1.5rem] text-[20px] text-[#ffffff]">
           We have experts available to provide guidance for you.
         </p>
-        <div className="flex  mt-[2rem] gap-5">
+        <div className="flex lg:flex-row flex-col mt-[2rem] gap-5">
           <InputCmp type="text" label="Enter Your Name" />
           <InputCmp type="Number" label="Enter Your Number" />
           <InputCmp type="email" label="Enter Your email" />
@@ -23,7 +23,7 @@ const GetConsultant = () => {
         <div className="mt-[2rem]">
           <TextareaCmp />
         </div>
-        <div className="right flex-1 flex justify-start items-center mt-[2rem]">
+        <div className="right flex-1 flex justify-start items-center lg:mt-[2rem] mt-[20px]">
           <button className="py-[17px] px-[39px]  bg-[#34afa9]  hover:text-white font-bold">
             Submit
           </button>
